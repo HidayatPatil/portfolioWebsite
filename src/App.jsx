d98857f6dec7fac9253a95ca59127471d4d38540
HidayatPatil <hpatil2@my.bcit.ca>
2026-01-15 12:54:31 -0800

    message updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-export default function App(){
-  return(
+export default function App() {
+  return (
     <div>
-      <h1>Portfolio Under Progress</h1>
+      <h1>Portfolio Under Construction</h1>
+      <h4>This portfolio is currently being updated with selected projects.</h4>
+      <h4>
+        If you are a hiring manager or recruiter, feel free to reach out at{" "}
+        <b>hidayatpatil1@gmail.com</b>.
+      </h4>
+      <h4>
+        I’m happy to share project details, case studies, or private links upon
+        request.
+      </h4>
+      <h4>Thank you for your time and interest.</h4>
     </div>
-  )
+  );
 }
