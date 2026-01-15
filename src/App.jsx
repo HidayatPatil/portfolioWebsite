af37850e79bdf7f0dc4da46ebbcc0d7920dc7bf8
HidayatPatil <hpatil2@my.bcit.ca>
2026-01-15 15:14:53 -0800

    updated message and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 export default function App() {
   return (
-    <div>
-      <h1>Portfolio Under Construction</h1>
-      <h4>This portfolio is currently being updated with selected projects.</h4>
-      <h4>
-        If you are a hiring manager or recruiter, feel free to reach out at{" "}
-        <b>hidayatpatil1@gmail.com</b>.
-      </h4>
-      <h4>
-        I’m happy to share project details, case studies, or private links upon
-        request.
-      </h4>
-      <h4>Thank you for your time and interest.</h4>
+    <div className="mainSection">
+      <div className="firstSection">
+        <div className="underProgress">
+        <div className="progress-bar"></div>
+      </div>
+        <h1>Portfolio Under Construction</h1>
+        <h2>
+          This portfolio is currently being updated with selected projects.
+        </h2>
+      </div>
+      <div className="secondSection">
+        <h4>
+          If you are a hiring manager or recruiter, feel free to reach out at{" "}
+          <b>hidayatpatil1@gmail.com</b>.
+        </h4>
+        <h4>
+          I’m happy to share project details, case studies, or private links
+          upon request. Thank you for your time and interest.
+        </h4>
+      </div>
     </div>
   );
 }
